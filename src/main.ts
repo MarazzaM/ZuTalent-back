f8b3de2d42c650b9e805c9dec567eaf1ad3733b7
MarazzaM <marazza.matias@gmail.com>
2024-10-02 12:36:08 -0300

    Allow all origins for now, just to test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zutalent.vercel.app/'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
