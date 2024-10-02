387c55d4e69dbc154f884b3a0fd7c661c1461885
MarazzaM <marazza.matias@gmail.com>
2024-10-02 13:36:16 -0300

    use env instead of hardcoded min

--- src/pod/pod.service.ts
+++ src/pod/pod.service.ts
@@ -5,6 +5,7 @@ import { ScoreService } from '../score/score.service';
 @Injectable()
 export class PodService {
   private readonly ZUPASS_SIGNING_KEY = process.env.ZUPASS_SIGNING_KEY;
+  private readonly MIN_REQUIRED_SCORE = Number(process.env.MIN_REQUIRED_SCORE) || 20;
 
   constructor(private readonly scoreService: ScoreService) {}
 
@@ -15,8 +16,8 @@ export class PodService {
     try {       
       // Check the wallet's score
       const passportData = await this.scoreService.getPassportById(wallet);
-      if (passportData.passport.score < 20) {
-        throw new BadRequestException('Insufficient score: Minimum required score is 20');
+      if (passportData.passport.score < this.MIN_REQUIRED_SCORE) {
+        throw new BadRequestException(`Insufficient score: Minimum required score is ${this.MIN_REQUIRED_SCORE}`);
       }
       
       const pod = POD.sign(
